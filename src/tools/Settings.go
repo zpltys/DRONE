8702520ae3e057d3bc55c8294b8659fc3ae28411
zpltys <zpltys@163.com>
2019-07-24 19:27:37 +0800

    twitter hash

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -5,7 +5,7 @@ const (
 	//ResultPath = "./"
 	//NFSPath = "/home/xwen/graph/16/"
 	//NFSPath = "/data/xwen/webbase"
-	NFSPath = "/mnt/sdb1/zhangshuai/webbase_hash_32/"
+	NFSPath = "/slurm/zhangshuai/twitter_hash_32_DRONE/"
 
 
 	WorkerOnSC = false
