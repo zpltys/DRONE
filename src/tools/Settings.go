44030147523f292018052423f7611caadc78db15
zpltys <zpltys@163.com>
2019-08-01 15:36:23 +0800

    sdf

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -2,11 +2,11 @@ package tools
 
 const (
 	ResultPath = "/mnt/sdb1/zhangshuai/result/"
-	NFSPath = "/mnt/sdb1/zhangshuai/webbase_cdbh_32/"
+	NFSPath = "/mnt/nfs/xwen/liveJournal_vc_part12/"
 	//NFSPath = "../test_data/"
 	WorkerOnSC = false
 
-	ConfigPath = "config32.txt"
+	ConfigPath = "config12.txt"
 	//PatternPath = "../test_data/pattern.txt"
 	PatternPath = "pattern.txt"
 	GraphSimulationTypeModel = 100
