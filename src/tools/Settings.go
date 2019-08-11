fbf5ec85d0a1301f9787855532552bc1440bd3e6
zpltys <zpltys@163.com>
2019-08-11 11:44:26 +0800

    df

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -2,11 +2,11 @@ package tools
 
 const (
 	ResultPath = "/mnt/sdb1/zhangshuai/result/"
-	NFSPath = "/mnt/nfs/xwen/liveJournal_vc_part12/"
+	NFSPath = "/slurm/zhangshuai/twitter_cdbh_32_DRONE/"
 	//NFSPath = "../test_data/"
 	WorkerOnSC = false
 
-	ConfigPath = "config12.txt"
+	ConfigPath = "config32.txt"
 	//PatternPath = "../test_data/pattern.txt"
 	PatternPath = "pattern.txt"
 	GraphSimulationTypeModel = 100
