34e124f76c6da73082618f304449aba0e0350d72
zpltys <zpltys@163.com>
2019-08-20 22:27:40 +0800

    change path

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -2,11 +2,11 @@ package tools
 
 const (
 	ResultPath = "/mnt/sdb1/zhangshuai/result/"
-	NFSPath = "/mnt/nfs/zpltys/sssp_vc_part12/"
+	NFSPath = "/mnt/nfs/zpltys/lj_vc_part4/"
 	//NFSPath = "../test_data/"
 	WorkerOnSC = false
 
-	ConfigPath = "config12.txt"
+	ConfigPath = "config4.txt"
 	//PatternPath = "../test_data/pattern.txt"
 	PatternPath = "pattern.txt"
 	GraphSimulationTypeModel = 100
